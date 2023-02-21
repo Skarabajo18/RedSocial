c8208cf8fd8554bb2b75ab843bdda3d6def7ed7b
Skarabajo18 <felipesegovia14@gmail.com>
2023-02-21 05:56:43 -0300

    front

--- Cliente/cliente/src/App.js
+++ Cliente/cliente/src/App.js
@@ -1,11 +1,29 @@
-import './App.css';
-import User from './Components/prueba';
+import Form from './Components/Registro';
+import Navbar from './Components/navbar';
+import Carousel from './Components/carousel';
+import Card  from './Components/card';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
 
 function App() {
   return (
     <div className="App">
-      <h1>Conexion</h1>
-      <User/>
+      <Navbar/>
+      <Carousel/>
+      <Container>
+      <Row>
+      <Col xs={6} md={4}>
+        <Form/>
+      </Col>
+      </Row>
+      </Container>
+      
+      <Container>
+      <Card/><p></p><Card/>
+      </Container>
+      
+      
     </div>
   );
 }
