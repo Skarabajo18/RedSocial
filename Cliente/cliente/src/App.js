b521e712dad4d078f484f187b93d3d43123e72f3
Skarabajo18 <felipesegovia14@gmail.com>
2023-02-25 02:17:42 -0300

    version1

--- Cliente/cliente/src/App.js
+++ Cliente/cliente/src/App.js
@@ -6,17 +6,19 @@ import {
   
 } from "react-router-dom";
 
+import Navbar  from './components/Navbar';
 
 
 function App() {
   return (
     <Router>
+      <Navbar/>
       <div className="container">
           <hr/>
         <Routes>
         <Route path="/" element={<h1>pagina de inicio </h1>}/>
-        <Route path="/contacto2" element={<h1>pagina de contacto2</h1>}/>
-        <Route path="/contacto3" element={<h1>pagina de contacto3</h1>}/>
+        <Route path="/admin" element={<h1>Admin</h1>}/>
+        <Route path="/login" element={<h1>login</h1>}/>
         </Routes>
       </div>
     </Router>
@@ -24,3 +26,33 @@ function App() {
 }
 
 export default App;
+
+
+// // import React from "react";
+// // import {Container, Row, Col, Card, Form, Button } from "react-bootstrap";
+// // import { withRouter } from "react-router";
+
+// // import './Styles/App.css'
+
+// const Dash = props => {
+   
+
+//     return (
+//         <>
+//          <Container fluid>
+//                 <Row>
+//                     <Col xs={2} id="sidebar-wrapper">      
+//                       <Sidebar />
+//                     </Col>
+//                     <Col  xs={10} id="page-content-wrapper">
+//                         this is a test
+//                     </Col> 
+//                 </Row>
+
+//             </Container>
+//         </>
+//         );
+//   };
+//   const Dashboard = BrowserRouter(Dash);
+//   export default Dashboard
+
--- Cliente/cliente/src/Components/navbar.js
+++ Cliente/cliente/src/Components/navbar.js
@@ -1,51 +1,40 @@
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-function NavScrollExample() {
+function CollapsibleExample() {
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#buscador">Buscador</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features"> Caracteristicas</Nav.Link>
+            <Nav.Link href="#pricing">Chat</Nav.Link>
+            <NavDropdown title="Configuraciones" id="collasible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Editar perfil</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                callaolacra
               </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
               </NavDropdown.Item>
             </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Premium</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              Red Social
             </Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default NavScrollExample;
+export default CollapsibleExample;
